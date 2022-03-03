e9e4c77e040736b04599733d62e3e4c8a564eb03
Anish kumar <94589270+Anishkumar31@users.noreply.github.com>
2022-03-03 03:19:45 +0000

    p4 done

--- p4final.c
+++ p4final.c
@@ -1,1 +1,35 @@
-
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("Enter the number\n");
+  scanf("%d",&n);
+  return n;
+}
+int find_fibo(int n)
+{
+  int fibo;
+  int num1=0;
+  int num2=1;
+  fibo=num1+num2;
+  for(int i=2;i<n;++i)
+    {
+      printf("%d",fibo);
+      num1=num2;
+      num2=fibo;
+      fibo=num1+num2;
+    }
+  return fibo;
+}
+void output(int n,int fibo)
+{
+  printf("\nfibonacci for %d is %d",n,fibo);
+}
+int main()
+{
+  int n,fibo;
+  n=input();
+  fibo=find_fibo(n);
+  output(n,fibo);
+  return 0;
+}
